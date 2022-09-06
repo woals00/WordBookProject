ac8cb47acade7899dc3ec6c4f3556456ce705e56
jaeminjeong <21900675@handong.edu>
2022-09-06 16:30:22 +0900

    create 4class and 1 interface.

---
+++ src/main/java/org/example/Word.java
@@ -0,0 +1,55 @@
+package org.example;
+
+public class Word {
+        private int id;
+        private int level;
+        private String word;
+        private String meaning;
+
+        Word(){}
+        Word(int id, int level, String word, String meaning){
+            this.id = id;
+            this.level = level;
+            this.word = word;
+            this.meaning = meaning;
+        }
+
+        public int getId() {
+            return id;
+        }
+        public void setId(int id) {
+            this.id = id;
+        }
+        public int getLevel() {
+            return level;
+        }
+        public void setLevel(int level) {
+            this.level = level;
+        }
+        public String getWord() {
+            return word;
+        }
+        public void setWord(String word) {
+            this.word = word;
+        }
+        public String getMeaning() {
+            return meaning;
+        }
+        public void setMeaning(String meaning) {
+            this.meaning = meaning;
+        }
+
+
+        @Override
+        public String toString() {
+            // TODO Auto-generated method stub
+            String slevel = "";
+            for(int i = 0; i < level; i++) {
+                slevel += "*";
+            }
+            String str = String.format("%3s", slevel)
+                    + String.format("%-10s", slevel) + word + " " + meaning;
+            return str;
+        }
+}
+
---
+++ src/main/java/org/example/WordManager.java
@@ -0,0 +1,46 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class WordManager {
+
+    WordCRUD wordCRUD;
+    Scanner s = new Scanner (System.in);
+
+    public int selectOption() {
+
+        System.out.print("*** 영단어 마스터 ***\n"
+                + "******************\n"
+                + "1. 모든 단어보기\n"
+                + "2. 수준별 단어 보기\n"
+                + "3. 단어 검색\n"
+                + "4. 단어 추가\n"
+                + "5. 단어 수정\n"
+                + "6. 단어 삭제\n"
+                + "7. 파일 저장\n"
+                + "0. 나가기\n"
+                + "******************\n"
+                + "=> 원하는 옵션은? ");
+
+        return s.nextInt();
+    }
+
+    WordManager(){
+        wordCRUD = new WordCRUD(s);
+    }
+
+
+    public void start() {
+        while(true) {
+            int option = selectOption();
+            if(option == 0) break;
+            if(option == 4) {
+                wordCRUD.addWord();
+            }
+            else if(option == 1) {
+                wordCRUD.listAll();
+            }
+        }
+//		System.out.println(option);
+    }
+}
